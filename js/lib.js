8298529fd88ddbca676f1a12d2720fe997c10b0b
freshervn <40496285+freshervn@users.noreply.github.com>
2020-10-28 11:57:41 +0700

    allmost done

--- js/lib.js
+++ js/lib.js
@@ -1,7 +1,22 @@
 
 function changeCss () {
-  var bodyElement = document.querySelector("body");
-  var navElement = document.querySelector("nav");
-  this.scrollY > document.querySelector('header').offsetWidth ? navElement.style.display = 'none': navElement.style.opacity = 1;
+	var bodyElement = document.querySelector("body");
+	var navElement = document.querySelector(".contain-nav");
+	var navBrand = document.querySelector(".navbar-brand");
+	var navLink = document.querySelectorAll(".nav-link");
+	var navToggler = document.querySelector(".navbar-toggler");
+
+	
+	if(this.scrollY > 1) {
+		navElement.style.background= '#344258';
+		navToggler.style.color=" color: rgb(255 254 254 / 50%);";	
+	}
+	else{ 
+		navElement.style.background= 'transparent';
+		navToggler.style.color="rgba(255,255,255,1)";
+		for (var i = 0; i < navLink.length; i++) {
+			navLink[i].style.color="white ";
+		}
+	}
 }
 window.addEventListener("scroll", changeCss , false);

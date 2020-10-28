48b4b6f35e5f098cef8b3edbe73446796f484c1b
freshervn <40496285+freshervn@users.noreply.github.com>
2020-10-28 21:59:39 +0700

    xong

--- js/lib.js
+++ js/lib.js
@@ -1,12 +1,9 @@
 
 function changeCss () {
-	var bodyElement = document.querySelector("body");
 	var navElement = document.querySelector(".contain-nav");
 	var navBrand = document.querySelector(".navbar-brand");
 	var navLink = document.querySelectorAll(".nav-link");
 	var navToggler = document.querySelector(".navbar-toggler");
-
-	
 	if(this.scrollY > 1) {
 		navElement.style.background= '#344258';
 		navToggler.style.color=" color: rgb(255 254 254 / 50%);";	
